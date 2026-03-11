58d7dbf3c647c5b1a93798fb736b2c29f7bbc451
Priya-Satpathy <priyasatpathy429@gmail.com>
2026-03-12 00:23:54 +0530

    content added

--- 1003/script.js
+++ 1003/script.js
@@ -1,6 +1,11 @@
 
-
-for(let b of "Harry"){
-    console.log(b);
-    
+// for(let b of "Harry"){
+//     console.log(b); 
+// }
+let n,i=0;
+let a = prompt("Enter the value of n");
+n = Number.parseInt(a);
+while(i<n){
+    console.log(i);
+    i++;  
 }
