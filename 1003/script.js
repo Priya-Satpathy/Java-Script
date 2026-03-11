eb3a4b578b32f8b9a0929838b73ec50ffb3b660c
Priya-Satpathy <priyasatpathy429@gmail.com>
2026-03-12 00:36:30 +0530

    content added

--- 1003/script.js
+++ 1003/script.js
@@ -2,10 +2,33 @@
 // for(let b of "Harry"){
 //     console.log(b); 
 // }
-let n,i=0;
-let a = prompt("Enter the value of n");
-n = Number.parseInt(a);
-while(i<n){
-    console.log(i);
-    i++;  
+
+// let n,i=0;
+// let a = prompt("Enter the value of n");
+// n = Number.parseInt(a);
+// while(i<n){
+//     console.log(i);
+//     i++;  
+// }
+
+// let a = prompt("Enter the value of n");
+// n = Number.parseInt(a);
+// let i = 10;
+// do{
+//     console.log(i);
+//     i++;  
+
+// }while(i<n);
+
+function onePlusAvg(x,y){
+    return 1+(x+y)/2;
 }
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log("One Plus Average of a and b is",onePlusAvg(a,b));
+
+console.log("One Plus Average of b and c is",onePlusAvg(b,c));
+
+console.log("One Plus Average of b and c is",onePlusAvg(a,c));
