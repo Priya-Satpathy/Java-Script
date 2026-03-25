31e5170cbd44b9b69a63a06fce2aa5232ff5ca57
Priya-Satpathy <priyasatpathy429@gmail.com>
2026-03-25 09:51:32 +0530

    content added

---
+++ 1003/script1.js
@@ -0,0 +1,17 @@
+let fr = "Harry";
+console.log(fr.length);
+
+console.log(fr.toUpperCase());
+console.log(fr.toLowerCase());
+
+console.log(fr.slice(2,4));
+console.log(fr.slice(4));
+
+console.log(fr.replace("Harr","Perr"));
+let friend = "Carry";
+console.log(fr.concat(" is a friend of ", friend,  " ok" ));
+
+let sur = "     Shimandi   "
+console.log(sur.trim());
+console.log(sur);
+
