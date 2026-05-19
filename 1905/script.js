d54946e15d8762b20cccb7a46242227699468a65
Priya-Satpathy <priyasatpathy429@gmail.com>
2026-05-19 21:58:32 +0530

    content added

---
+++ 1905/script.js
@@ -0,0 +1,130 @@
+// addition();
+
+// function addition(){
+//     let a = parseInt(prompt("Enter first number:"));
+//     let b = parseInt(prompt("Enter second number:"));
+//     let s = a+b;
+//     console.log("Sum is ",s);
+    
+// }
+
+// function addition(){
+//     let a = parseInt(prompt("Enter first number:"));
+//     let b = parseInt(prompt("Enter second number:"));
+//     let s = a+b;
+//    return s;
+    
+// }
+// let s = addition();
+//  console.log("Sum is ",s);
+
+// function addition(a,b){
+//     let s = a+b;
+//     console.log("Sum is ",s);   
+// }
+// let a = parseInt(prompt("Enter first number:"));
+// let b = parseInt(prompt("Enter second number:"));
+// let s = addition(a,b);
+
+// function addition(a,b){
+//     let s = a+b;
+//     return s;   
+    
+// }
+// let a = parseInt(prompt("Enter first number:"));
+// let b = parseInt(prompt("Enter second number:"));
+// let s = addition(a,b);
+// console.log("Sum is ",s);
+
+
+// function Expression
+// const addition = function(){
+//     let a = parseInt(prompt("Enter first number:"));
+//     let b = parseInt(prompt("Enter second number:"));
+//     let s = a+b;
+//     console.log("Sum is ",s);
+    
+// }
+// addition();
+
+// const addition = function(){
+//     let a = parseInt(prompt("Enter first number:"));
+//     let b = parseInt(prompt("Enter second number:"));
+//     let s = a+b;
+//    return s;
+    
+// }
+// let s = addition();
+//  console.log("Sum is ",s);
+
+
+//  const addition = function(a,b){
+//     let s = a+b;
+//     console.log("Sum is ",s);
+    
+    
+// }
+// let a = parseInt(prompt("Enter first number:"));
+// let b = parseInt(prompt("Enter second number:"));
+// let s = addition(a,b);
+
+// const  addition = function(a,b){
+//     let s = a+b;
+//     return s;   
+    
+// }
+// let a = parseInt(prompt("Enter first number:"));
+// let b = parseInt(prompt("Enter second number:"));
+// let s = addition(a,b);
+// console.log("Sum is ",s);
+
+// Arrow Function
+
+// const addition = ()=>{
+//     let a = parseInt(prompt("Enter first number:"));
+//     let b = parseInt(prompt("Enter second number:"));
+//     let s = a+b;
+//     console.log("Sum is ",s);
+    
+// }
+// addition();
+
+// const addition = ()=>{
+//     let a = parseInt(prompt("Enter first number:"));
+//     let b = parseInt(prompt("Enter second number:"));
+//     let s = a+b;
+//    return s;
+    
+// }
+// let s = addition();
+//  console.log("Sum is ",s);
+
+
+//  const addition = (a,b)=>{
+//     let s = a+b;
+//     console.log("Sum is ",s);
+    
+    
+// }
+// let a = parseInt(prompt("Enter first number:"));
+// let b = parseInt(prompt("Enter second number:"));
+// let s = addition(a,b);
+
+// const addition = ()=>{
+//     let a = parseInt(prompt("Enter first number:"));
+//     let b = parseInt(prompt("Enter second number:"));
+//     let s = a+b;
+//    return s;
+    
+// }
+// let s = addition();
+//  console.log("Sum is ",s);
+
+
+    
+    
+
+
+
+
+
